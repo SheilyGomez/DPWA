25ff68c54c1bc3c91fef50439a4d49c9009a749c
florence <florencevasquez2016@outlook.com>
2025-02-22 16:40:40 -0600

    Ejercicio 1 - Formulario

---
+++ primerTrabajoGrupal/Formulario.aspx
@@ -0,0 +1,10 @@
+﻿<%@ Page Title="Formulario" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Formulario.aspx.cs" Inherits="primerTrabajoGrupal.Formulario" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h2>Ingrese un texto</h2>
+
+    <asp:TextBox ID="txtMensaje" runat="server" placeholder="Escriba algo aquí" CssClass="form-control"></asp:TextBox>
+    <br />
+    <asp:Button ID="btnMostrar" runat="server" Text="Mostrar Mensaje" OnClick="btnMostrar_Click" CssClass="btn btn-primary"/>
+
+</asp:Content>
---
+++ primerTrabajoGrupal/Formulario.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Web.UI;
+
+namespace primerTrabajoGrupal
+{
+    public partial class Formulario : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void btnMostrar_Click(object sender, EventArgs e)
+        {
+            string mensaje = txtMensaje.Text;
+
+            if (!string.IsNullOrEmpty(mensaje))
+            {
+               
+                string script = $@"
+                    Swal.fire({{
+                        title: 'Mensaje',
+                        text: '{mensaje}',
+                        icon: 'success'
+                    }});";
+
+                
+                ScriptManager.RegisterStartupScript(this, GetType(), "SweetAlert", script, true);
+            }
+        }
+    }
+}
